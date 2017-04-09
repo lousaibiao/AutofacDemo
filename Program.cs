735f0cbd3980586545d4a5d6940af1bd059e5f47
lousaibiao <lousaibiao@live.cn>
2017-04-09 22:48:57 +0800

    完成一个

--- Program.cs
+++ Program.cs
@@ -52,6 +52,13 @@ namespace autofac
   // WriteDate such that today's date is written out;
   // you could have one that writes in a different format
   // or a different date.
+  /*
+  TodayWriter这个里汇聚了所有东西。
+  注意一下他把IOutput这个接口作为自己构造函数的参数。
+  这使得这个writer可以根据IOutput这个接口的具体实现向任何地方做输出操作。
+  进一步来说，实现了writedata所以今天得日期被打印出来。
+  你可以自己实现一个其他的，以不同的形式，或者不同的日期。
+   */
   public class TodayWriter : IDateWriter
   {
     private IOutput _output;
